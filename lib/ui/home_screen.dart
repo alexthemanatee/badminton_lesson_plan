e667ba70ecf83bc204e84273cdc5d8df18809ca6
Brian Duong <b7duong@uwaterloo.ca>
2020-03-05 19:42:23 -0500

    New pages added

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,16 +1,33 @@
 import 'package:badminton_lesson_plan/ui/warmup_screen.dart';
+import 'package:badminton_lesson_plan/ui/fields_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatelessWidget {
+
+  final TextEditingController _durationController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text("Badminton Lesson Plan"),
         ),
-        body: Center(
+        body: Padding(
+            padding: const EdgeInsets.all(30.0),
             child: Column(
           children: <Widget>[
+            TextField(
+              textAlign: TextAlign.center,
+              controller: _durationController,
+              decoration: InputDecoration(
+                hintText: 'Input Class Duration in Minutes'
+              ),
+              keyboardType: TextInputType.number,
+              inputFormatters: <TextInputFormatter>[
+                WhitelistingTextInputFormatter.digitsOnly
+              ],
+            ),
             FlatButton(
               child: Text("Click Me"),
               onPressed: () {
@@ -23,6 +40,6 @@ class HomeScreen extends StatelessWidget {
 
   Future navigateToSubPage(context) async {
     Navigator.push(
-        context, MaterialPageRoute(builder: (context) => WarmupScreen(minutes: 5)));
+        context, MaterialPageRoute(builder: (context) => FieldScreen(int.parse(_durationController.text))));
   }
 }
