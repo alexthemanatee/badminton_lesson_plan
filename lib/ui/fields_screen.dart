06a5dc3a724340074c4196b430a240e40abd52cb
Brian Duong <b7duong@uwaterloo.ca>
2020-03-05 20:29:19 -0500

    Update fields_screen.dart

--- lib/ui/fields_screen.dart
+++ lib/ui/fields_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 import 'package:badminton_lesson_plan/ui/drill_screen.dart';
 
@@ -14,34 +15,47 @@ class FieldScreen extends StatefulWidget {
 }
 
 class FieldScreenState extends State<FieldScreen> {
-
   int total;
+  final TextEditingController _wuController = TextEditingController();
+  final TextEditingController _fwController = TextEditingController();
+  final TextEditingController _serveController = TextEditingController();
+  final TextEditingController _gameController = TextEditingController();
 
   FieldScreenState(this.total);
 
   @override
-
   Widget build(BuildContext context) {
-    return Scaffold (
-      appBar: AppBar(
-        title: Text('Enter Fields:')
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: Column(
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.fromLTRB(0, 0, 16, 4),
-              child: Text('Unallocated Time:  ' + total.toString() + '  minutes',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 15,
+    return Scaffold(
+        appBar: AppBar(title: Text('Enter Fields:')),
+        body: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.fromLTRB(0, 0, 16, 4),
+                child: Text(
+                  'Unallocated Time:  ' + total.toString() + '  minutes',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15,
+                  ),
                 ),
               ),
-            ),
-          ],
-        ),
-      )
-    );
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('Warmup Time:'),
+                  TextField(
+                    controller: _wuController,
+                    keyboardType: TextInputType.number,
+                    inputFormatters: <TextInputFormatter>[
+                      WhitelistingTextInputFormatter.digitsOnly,
+                    ]
+                  )
+                ],
+              )
+            ],
+          ),
+        ));
   }
 }
